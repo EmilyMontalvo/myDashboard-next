7e0c0e77dc2b91a5ce8b5fdb22cefef5535c4408
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-04-20 17:25:30 -0500

    feat: local storage bad practice

--- src/store/pokemons/pokemonsSlice.ts
+++ src/store/pokemons/pokemonsSlice.ts
@@ -3,10 +3,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface PokemonsFavState {
   [key: string]: SimplePokemon
-  //! Voy a tener x cantidad de llaves, pero esas llaves van a ser strings
-  //! Cada una de esas llaves apuntan a un valor de tipo SimplePokemon
+  // Voy a tener x cantidad de llaves, pero esas llaves van a ser strings
+  // Cada una de esas llaves apuntan a un valor de tipo SimplePokemon
 }
+
+//! Esto si es permitido
+const getInitialState = (): PokemonsFavState => { //Aquí extraigo del local storage
+  const favorites = JSON.parse(localStorage.getItem('favorite-pokemons') ?? '{}');
+  return favorites
+}
+
 const initialState: PokemonsFavState = {
+  ...getInitialState(),
   // '1': { id: '1', name: 'bulbasaur' },
   // '5': { id: '5', name: 'bulbasaur' }
 }
@@ -22,13 +30,18 @@ const pokemonsSlice = createSlice({
       //Cuando lo quito de favoritos
       if (!!state[id]) {
         delete state[id]
-        return;
+        // return;
+      } else {
+        state[id] = pokemon;
       }
-      state[id] = pokemon;
+      //! Lo que se va a hacer es una mala práctica pero es por uso educativo sobre el local storage
+      //!Esto no se debe de hacer en redux
+      localStorage.setItem('favorite-pokemons', JSON.stringify(state))
+
     }
   }
 });
 
-export const { toggleFavorite} = pokemonsSlice.actions
+export const { toggleFavorite } = pokemonsSlice.actions
 
 export default pokemonsSlice.reducer
