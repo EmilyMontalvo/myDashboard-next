c71e7695b920db4e8d430d8cd8a8c3f8ff4fda3c
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-04-20 00:54:28 -0500

    feat: redux configuration

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,11 +1,17 @@
 import { SimpleWidget } from '@/components'
 import React from 'react'
 
+
+export const metadata = {
+ title: 'Admin dashboard',
+ description: 'Admin Dashboard',
+};
+
 const page = () => {
   return (
     <div className='text-black p-2'>
-      <h1 className='mt-2 text-3xl'>Dashboard</h1>
-      <span className=''>Información general</span>
+      <h1 className='mt-2 text-3xl text-center'>Dashboard</h1>
+      <span>Información general</span>
       <div className='flex flex-wrap p-2 mt-5'>
         <SimpleWidget/>
       </div>
